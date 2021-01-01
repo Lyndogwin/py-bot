e17167d8019e3bdd15140a99eb558f3cde30667c
Brandon B <brand949@gmail.com>
2021-01-01 12:18:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ class MyClient(discord.Client):
         print('Message from {0.author}: {0.content}'.format(messaage))
 
 client = MyClient()
-client.run('Nzk0MzI5OTIxODEwOTg5MDg2.X-5PYw.ADWXwgU7G-3n1EhuX0zc-EudUG8')
+client.run('Bot Token')
